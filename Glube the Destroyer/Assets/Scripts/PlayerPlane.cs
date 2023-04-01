361e332b3d809d64c92fa5b16330aeab184a2c57
zeinging <popcorn.whitaker7@gmail.com>
2023-03-31 19:23:00 -0600

    Better Plane Controls

--- Glube the Destroyer/Assets/Scripts/PlayerPlane.cs
+++ Glube the Destroyer/Assets/Scripts/PlayerPlane.cs
@@ -35,21 +35,28 @@ public class PlayerPlane : MonoBehaviour
 
         Vector2 inputVector = playerInputActions.Player.Movement.ReadValue<Vector2>();//read control inputs
 
-        var step = turnSpeed * Time.deltaTime;
+        var UDStep = turnSpeed * Time.deltaTime;
+        
 
-        float Xtemp = transform.localEulerAngles.y + inputVector.x * speed;
-        Quaternion XAngle = Quaternion.AngleAxis(inputVector.y * 45, Vector3.right);//clamp plane up and down rotation
-        XAngle = Quaternion.Euler(XAngle.eulerAngles.x, Xtemp, XAngle.eulerAngles.z);//add in Right and left rotation
-        Quaternion XLevel = Quaternion.Euler(0, Xtemp, transform.localEulerAngles.z);//level plane rotation
+        float XMov = Mathf.MoveTowards(transform.localEulerAngles.y, transform.localEulerAngles.y + turnSpeed, inputVector.x);
+        float Xtemp = transform.localEulerAngles.y + inputVector.x * 0.5f;
+        
+        //Debug.Log(inputVector.x);
+
+        //Quaternion XAngle = Quaternion.AngleAxis(inputVector.y * 45, Vector3.right);//clamp plane up and down rotation
+        //Quaternion ZAngle = Quaternion.AngleAxis(inputVector.x * 10, Vector3.up);
+        Quaternion XAngle = Quaternion.Euler(45 * inputVector.y, Xtemp, -30 * inputVector.x);//add in Right and left rotation
+        
+        Quaternion XLevel = Quaternion.Euler(0, Xtemp, 0);//level plane rotation
 
 
         if(inputVector.magnitude != 0.2){
 
-            transform.localRotation = Quaternion.RotateTowards(transform.localRotation, XAngle, step);//rotate plane from inputs
+            transform.localRotation = Quaternion.RotateTowards(transform.localRotation, XAngle, UDStep);//rotate plane from inputs
             
 
         }else{
-            transform.localRotation = Quaternion.RotateTowards(transform.localRotation, XLevel, step);//level plane when inpus equal 0
+            transform.localRotation = Quaternion.RotateTowards(transform.localRotation, XLevel, UDStep);//level plane when inpus equal 0
         }
 
         // if(inputVector.y != 0){
