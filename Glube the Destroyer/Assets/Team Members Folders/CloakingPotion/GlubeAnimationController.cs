0f76680b078ad8e43488f11408ba4f23f1a0b026
zeinging <popcorn.whitaker7@gmail.com>
2023-04-02 22:31:17 -0600

    Glube Navigation Animations

--- Glube the Destroyer/Assets/Team Members Folders/CloakingPotion/GlubeAnimationController.cs
+++ Glube the Destroyer/Assets/Team Members Folders/CloakingPotion/GlubeAnimationController.cs
@@ -1,13 +1,36 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Assets.Team_Members_Folders.CloakingPotion
 {
+
     public class GlubeAnimationController : MonoBehaviour
     {
-        public void StartWalkingAnimationdDirectedTowards()
+        public Animator anim;
+
+        public float agentSpeedSLider = 0;
+
+        public NavMeshAgent myAgent;
+
+        private void Start(){
+            //anim.SetFloat("AgentSpeed", agentSpeedSLider);
+
+        }
+
+        void Update(){
+            agentSpeedSLider = Mathf.Clamp01(agentSpeedSLider);
+            float temp = Mathf.Clamp01(myAgent.velocity.magnitude);
+            anim.SetFloat("AgentSpeed", temp);
+            
+        }
+
+        public void StartWalkingAnimationdDirectedTowards(Vector3 targetDirection)
 
         {
+            //anim.CrossFade("Walk", 0.25f);
+            //anim.Play("Walk");
+            //Vector3 targetDirection = Quaternion.LookRotation();
         }
 
         public void StartAttackingAnimationDirectedAt()
